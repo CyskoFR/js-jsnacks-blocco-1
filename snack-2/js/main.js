8a2866af9a304c9fac445692061371eef39a02b6
CyskoFR <franc.sko91@gmail.com>
2022-05-04 12:00:59 +0200

    add else if

--- snack-2/js/main.js
+++ snack-2/js/main.js
@@ -4,6 +4,8 @@ const word2 = prompt ("inserisci seconda parola")
 if (word1.length > word2.length) {
     console.log(word2)
     console.log(word1)
+} else if (word1.length = word2.length) {
+    console.log("stessa lunghezza!")
 } else {
     console.log(word1)
     console.log(word2)
