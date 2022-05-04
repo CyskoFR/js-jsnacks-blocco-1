6b334dc62c71bf910aa26f803160e338870c5c84
CyskoFR <franc.sko91@gmail.com>
2022-05-04 12:53:37 +0200

    add push method

--- snack-5/js/main.js
+++ snack-5/js/main.js
@@ -1,9 +1,10 @@
-const array = []
+const array = [];
 
 for (let i =0; i < 6; i++) {
     const userNumber = Number(prompt("inserisci un numero"))
     if (userNumber % 2 === 1) {
-        array.splice(2, 0, userNumber);
+        array.splice(0, 0, userNumber);
+        // array.push(userNumber);
         console.log(array);
     }
 }
